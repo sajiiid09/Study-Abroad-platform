552515175400a7ddba469cba025a97eeb7cb30ec
Sajid Mahmud <134144226+sajiiid09@users.noreply.github.com>
2025-12-05 04:44:45 +0600

    chore: add backend skeleton

---
+++ src/config/env.js
@@ -0,0 +1,9 @@
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+module.exports = {
+  port: process.env.PORT || 5000,
+  databaseUrl: process.env.DATABASE_URL,
+  jwtSecret: process.env.JWT_SECRET,
+};
